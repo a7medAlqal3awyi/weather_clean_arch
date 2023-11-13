6eaba114dd491b52bd5ee66130565f030ff49619
Ahmed <a7medalqalawyi@bmail.com>
2023-11-13 20:54:18 +0200

    compete the domain layer

---
+++ lib/domain/repo/base_repo.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:weather_clean_arch/core/error/failure.dart';
+import 'package:weather_clean_arch/domain/entities/weather.dart';
+
+abstract class WeatherRepository {
+  Future<Either<Failure,Weather>>getWeatherByCity(String city);
+}
